0a4b5951ff00253a2660af6db1e35f195349005f
cvanelteren <caspervanelteren@gmail.com>
2020-10-22 01:16:31 +0200

    try except behavior for batch setters

--- lib/matplotlib/artist.py
+++ lib/matplotlib/artist.py
@@ -990,7 +990,12 @@ class Artist:
                     if not callable(func):
                         raise AttributeError(f"{type(self).__name__!r} object "
                                              f"has no property {k!r}")
-                    ret.append(func(v))
+                    # dealing when possible
+                    try:
+                        ret.append(func(**v))
+                    # default behavior
+                    except:
+                        ret.append(func(v))
         if ret:
             self.pchanged()
             self.stale = True
